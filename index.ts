19e0e434c05cab070d7ec1a8c029b25e73970ac4
Zain Hill <zainthedev@gmail.com>
2021-03-07 14:19:04 +0900

    Refactor with Express

--- index.ts
+++ index.ts
@@ -1,44 +1,25 @@
-import http = require('http');
-import fs = require('fs');
+import express = require('express');
+import path = require('path');
 
-http
-	.createServer((req, res) => {
-		if (req.url === '/') {
-			fs.readFile('index.html', (err, data) => {
-				if (err) {
-					throw err;
-				}
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.write(data);
-				return res.end();
-			});
-		} else if (req.url === '/about') {
-			fs.readFile('about.html', (err, data) => {
-				if (err) {
-					throw err;
-				}
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.write(data);
-				return res.end();
-			});
-		} else if (req.url === '/contact') {
-			fs.readFile('contact-me.html', (err, data) => {
-				if (err) {
-					throw err;
-				}
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.write(data);
-				return res.end();
-			});
-		} else {
-			fs.readFile('404.html', (err, data) => {
-				if (err) {
-					throw err;
-				}
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.write(data);
-				return res.end();
-			});
-		}
-	})
-	.listen(8080);
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(path.join(__dirname, 'contact-me.html'));
+});
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}!`);
+});
